1ba4f6beb63436be5a1fce67990a8d48dd2fe2e0
maximfersco <mr.maksim@mail.ru>
2021-01-07 01:25:35 +0700

    first commit

---
+++ jsname/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+//const answer = +prompt("Вам есть 18?","18");
+//console.log(answer+5);
+
+//const answer = []
+
+ //answer[0] = prompt("Вам есть 18?","");
+ //answer[1] = prompt('как вас зовут', '');
+ //answer[2] = prompt("Вам есть 18?","");
+
+//console.log(typeof(answer));
+
+
+        console.log(2*4===8);
+
+
+
+
+
+
+
+
+
